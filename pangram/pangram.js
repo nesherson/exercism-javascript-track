597979e6158f0db704efa58d66b5c0c1ef1f05ff
unknown <nesad.omeragich27@gmail.com>
2021-04-06 01:25:28 +0200

    Removed unnecessary code

--- pangram/pangram.js
+++ pangram/pangram.js
@@ -2,7 +2,6 @@
 // This is only a SKELETON file for the 'Pangram' exercise. It's been provided as a
 // convenience to get you started writing code faster.
 //
-//a b c d e f g h i j k l m n o p q r s t u v w x y z
 
 const alphabet = [
   'a',
