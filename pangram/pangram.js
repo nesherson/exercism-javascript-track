08be68c058fd62809bbfe28be343e842f30cf796
unknown <nesad.omeragich27@gmail.com>
2021-04-06 01:23:28 +0200

    Finished pangram with all test passed

---
+++ pangram/pangram.js
@@ -0,0 +1,48 @@
+//
+// This is only a SKELETON file for the 'Pangram' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+//a b c d e f g h i j k l m n o p q r s t u v w x y z
+
+const alphabet = [
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z',
+];
+
+export const isPangram = (word) => {
+  if (word.length < alphabet.length) {
+    return false;
+  }
+
+  const allLowerCaseWord = word.toLowerCase();
+
+  const isPangram = alphabet.every((letter) =>
+    allLowerCaseWord.includes(letter)
+  );
+
+  return isPangram;
+};
