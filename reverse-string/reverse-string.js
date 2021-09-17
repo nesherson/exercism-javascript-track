5320418d1258fbc87adbdb51f2bde006c7b0a4df
nesherson <nesad.omeragich27@gmail.com>
2021-09-18 01:21:40 +0200

    Finish reverse-string exercise with all test passed

--- reverse-string/reverse-string.js
+++ reverse-string/reverse-string.js
@@ -3,6 +3,9 @@
 // convenience to get you started writing code faster.
 //
 
-export const reverseString = () => {
-  throw new Error('Remove this statement and implement this function');
+// Without using Array.prototype.reverse() method
+export const reverseString = (str) => {
+  const tempArr = str.split('');
+  const len = tempArr.length - 1;
+  return tempArr.map((el, i, arr) => arr[len - i]).join('');
 };
