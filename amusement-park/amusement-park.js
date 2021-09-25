5b467f0bc52ccc1ad05fffe2a0fa0fdf66b83a83
nesherson <nesad.omeragich27@gmail.com>
2021-09-26 01:26:07 +0200

    Add initial exercise project structure

---
+++ amusement-park/amusement-park.js
@@ -0,0 +1,67 @@
+/// <reference path="./global.d.ts" />
+// @ts-check
+
+/**
+ * Creates a new visitor.
+ *
+ * @param {string} name
+ * @param {number} age
+ * @param {string} ticketId
+ * @returns {Visitor} the visitor that was created
+ */
+export function createVisitor(name, age, ticketId) {
+  return {
+    name,
+    age,
+    ticketId,
+  };
+}
+
+/**
+ * Revokes a ticket for a visitor.
+ *
+ * @param {Visitor} visitor the visitor with an active ticket
+ * @returns {Visitor} the visitor without a ticket
+ */
+export function revokeTicket(visitor) {
+  visitor.ticketId = null;
+  return visitor;
+}
+
+/**
+ * Determines the status a ticket has in the ticket tracking object.
+ *
+ * @param {Record<string, string|null>} tickets
+ * @param {string} ticketId
+ * @returns {string} ticket status
+ */
+export function ticketStatus(tickets, ticketId) {
+  for (let ticket in tickets) {
+    if (ticket === ticketId && tickets[ticket] !== null)
+      return `sold to ${tickets[ticket]}`;
+    else if (ticket === ticketId && tickets[ticket] === null) return 'not sold';
+  }
+  return 'unknown ticket id';
+}
+
+/**
+ * Determines the status a ticket has in the ticket tracking object
+ * and returns a simplified status message.
+ *
+ * @param {Record<string, string|null>} tickets
+ * @param {string} ticketId
+ * @returns {string} ticket status
+ */
+export function simpleTicketStatus(tickets, ticketId) {
+  throw new Error('Please implement the simpleTicketStatus function.');
+}
+
+/**
+ * Determines the version of the GTC that was signed by the visitor.
+ *
+ * @param {VisitorWithGtc} visitor
+ * @returns {string | undefined} version
+ */
+export function gtcVersion(visitor) {
+  throw new Error('Please implement the gtcVersion function.');
+}
