3894dad0e95a1450d4c87af506575ed8d53d1717
nesherson <nesad.omeragich27@gmail.com>
2021-10-19 23:17:26 +0200

    Finish exercise with three tests passed

--- lasagna-master/lasagna-master.js
+++ lasagna-master/lasagna-master.js
@@ -10,3 +10,36 @@
  *   ...
  * }
  */
+
+export function cookingStatus(timer) {
+  if (timer < 0) return;
+
+  if (timer === 0) return 'Lasagna is done.';
+
+  if (timer > 0) return 'Not done, please wait.';
+
+  return 'You forgot to set the timer.';
+}
+
+export function preparationTime(layers, layerPreparation = 2) {
+  let totalTime = 0;
+
+  layers.forEach((l) => {
+    totalTime += layerPreparation;
+  });
+  return totalTime;
+}
+
+export function quantities(layers) {
+  const totalQuantities = {
+    noodles: 0,
+    sauce: 0,
+  };
+
+  layers.forEach((l) => {
+    if (l === 'noodles') totalQuantities[l] += 50;
+    else if (l === 'sauce') totalQuantities[l] += 0.2;
+  });
+
+  return totalQuantities;
+}
